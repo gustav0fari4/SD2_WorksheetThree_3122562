31f34346e2b5c29f9075545f0d3c813a656584ff
Gustavo <160332176+gustav0fari4@users.noreply.github.com>
2025-03-07 20:08:17 +0000

    Integration test added to shapeTest class

--- src/griffith/ShapeTest.java
+++ src/griffith/ShapeTest.java
@@ -2,6 +2,8 @@ package griffith;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 class ShapeTest {
@@ -42,6 +44,44 @@ class ShapeTest {
 		assertEquals(3.0 + 4.0 + hypotenuse, triangle.perimeter(), EPSILON);
 		//Test the toString method
 		assertEquals("Shape: Right Angled Triangle, Base: 3.0, Height: 4.0", triangle.toString());
+	}
+	
+	//Integration Test
+	@Test
+	public void testShapesIntegration() {
+		//Create an arrayList of shapes
+		ArrayList<Shape> shapes = new ArrayList<>();
+		
+		//Add two of each subclass to the list
+		shapes.add(new Circle("Circle", 3.5));
+		shapes.add(new Circle("Circle", 5.0));
+		shapes.add(new Rhombus("Rhombus", 4.0, 5.0, 6.0));
+		shapes.add(new Rhombus("Rhombus", 6.0, 7.0, 8.0));
+		shapes.add(new RightAngledTriangle("Right Angled Triangle", 3.0, 4.0));
+		shapes.add(new RightAngledTriangle("Right Angled Triangle", 5.0, 12.0));
+		
+		//Loop through each shape and test area perimeter methods
+		for(Shape shape : shapes) {
+			if(shape instanceof Circle) {
+				Circle circle = (Circle) shape;
+				assertEquals(circle.area(), Math.PI * circle.getRadius() * circle.getRadius(), EPSILON);
+				assertEquals(circle.perimeter(), 2 * Math.PI * circle.getRadius(), EPSILON);
+			} else if(shape instanceof Rhombus) {
+				Rhombus rhombus = (Rhombus) shape;
+				assertEquals(rhombus.area(), (rhombus.getDiagonal1() * rhombus.getDiagonal2())/ 2.0, EPSILON);
+				assertEquals(rhombus.perimeter(), 4 * rhombus.getSide(), EPSILON);
+			} else if(shape instanceof RightAngledTriangle) {
+				RightAngledTriangle triangle = (RightAngledTriangle) shape;
+				double hypotenuse = Math.sqrt(triangle.getBase() * triangle.getBase() + triangle.getHeight() * triangle.getHeight());
+				assertEquals(triangle.area(), (triangle.getBase() * triangle.getHeight()) / 2, EPSILON);
+				assertEquals(triangle.perimeter(), triangle.getBase() + triangle.getHeight() + hypotenuse, EPSILON);
+			}
+			
+		}
+
+
+
+
 	}
 
 }
