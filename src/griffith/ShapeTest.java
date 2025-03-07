7d7e0acb3dba72add3efb9ef5ef6362ad13a74ec
Gustavo <160332176+gustav0fari4@users.noreply.github.com>
2025-03-07 19:37:43 +0000

    ShapeTest Unit test for rhombus added

--- src/griffith/ShapeTest.java
+++ src/griffith/ShapeTest.java
@@ -18,5 +18,17 @@ class ShapeTest {
 		//Test the toString method
 		assertEquals("Shape: Circle, Radius: 3.5", circle.toString());
 	}
+	
+	//Unit test for Rhombus
+	@Test
+	public void testRhombus() {
+		Rhombus rhombus = new Rhombus("Rhombus", 4.0, 5.0, 6.0);
+		//Test the area
+		assertEquals(5.0 * 6.0 / 2.0, rhombus.area(), EPSILON);
+		//Test the perimeter
+		assertEquals(4 * 4.0, rhombus.perimeter(), EPSILON);
+		//Test the toString method
+		assertEquals("Shape: Rhombus, Side: 4.0, Diagonal 1: 5.0, Diagonal 2: 6.0", rhombus.toString());
+	}
 
 }
