75c447e109fbebc20eafa354fb9f479389ac8a0c
Gustavo <160332176+gustav0fari4@users.noreply.github.com>
2025-03-07 11:19:02 +0000

    Working on the ShapeAbstractClass

--- src/griffith/Shape.java
+++ src/griffith/Shape.java
@@ -1,5 +1,30 @@
 package griffith;
 
 public abstract class Shape {
+	private String name;
+	
+	//COnstructor to the set name
+	public Shape(String name) {
+		this.name = name;
+	}
+	
+	//Getter
+	public String getName() {
+		return name;
+	}
+	
+	//Setter
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	//Abstract methods for area and perimeter
+	public abstract double area();
+	public abstract double perimeter();
+	
+	//toString method to print the name of the shape
+	@Override
+	public String toString() {
+		return "Shape: " + name;
+		}
 }
